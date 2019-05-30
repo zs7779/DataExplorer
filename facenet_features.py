5f31509553efa893aaa2be3c1cf239c814e347be
zs7779 <zs7779@hotmail.com>
2019-05-30 17:00:01 -0400

    all in one file

--- facenet_features.py
+++ facenet_features.py
@@ -8,9 +8,17 @@ def facenet_h5_to_tsv(feature_file):
         feature_a = feature_h5['feature_a'][::100]
         feature_b = feature_h5['feature_b'][::100]
         feature_c = feature_h5['feature_c'][::100]
-        labels = feature_h5['pains'][::100].round()
+        pspi = feature_h5['pspi'][::100].round()
+        au4 = feature_h5['Continuous Sampling-Brow'][::100].round()
+        au67 = feature_h5['Continuous Sampling-Orbit'][::100].round()
+        au910 = feature_h5['Continuous Sampling-Levator'][::100].round()
+        au43 = feature_h5['Continuous Sampling-Eyes Closed'][::100].round()
         subjects = feature_h5['subjects'][::100]
-        np.savetxt('labels.tsv', labels, fmt='%.4f', delimiter='\t', newline='\n')
+        np.savetxt('pspi.tsv', pspi, fmt='%.4f', delimiter='\t', newline='\n')
+        np.savetxt('au4.tsv', au4, fmt='%.4f', delimiter='\t', newline='\n')
+        np.savetxt('au67.tsv', au67, fmt='%.4f', delimiter='\t', newline='\n')
+        np.savetxt('au910.tsv', au910, fmt='%.4f', delimiter='\t', newline='\n')
+        np.savetxt('au43.tsv', au43, fmt='%.4f', delimiter='\t', newline='\n')
         np.savetxt('subjects.tsv', subjects, fmt='%d', delimiter='\t', newline='\n')
         np.savetxt('bottleneck.tsv', bottleneck, fmt='%.4f', delimiter='\t', newline='\n')
         np.savetxt('feature_a.tsv', feature_a, fmt='%.4f', delimiter='\t', newline='\n')
@@ -36,6 +44,6 @@ def facenet_h5_to_tsv(feature_file):
 
 lmk_file = 'C:/Users/zhaosh/pain/artifacts/landmarks/LM_2019-04-24-18-37-28.hdf5'
 annot_file = 'C:/Users/zhaosh/pain/artifacts/annotations/annot_2019-04-17-18-04-39.hdf5'
-feature_file = 'C:/Users/zhaosh/pain/artifacts/facenet_features/facenet_features_2019-05-28-13-12-40.hdf5'
+feature_file = 'C:/Users/zhaosh/pain/artifacts/facenet_features/facenet_features_2019-05-28-17-23-28.hdf5'
 # labels = get_labels(lmk_file, annot_file)
 facenet_h5_to_tsv(feature_file)
